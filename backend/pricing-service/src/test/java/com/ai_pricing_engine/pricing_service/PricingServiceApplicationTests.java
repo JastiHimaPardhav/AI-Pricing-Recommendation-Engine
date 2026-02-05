60cce820777db4c66fc50e9811f37071003b7a15
JastiHimaPardhav <himapardhavjasti@gmail.com>
2026-02-05 23:29:52 +0530

    Added boiler plate for pricing-service

---
+++ backend/pricing-service/src/test/java/com/ai_pricing_engine/pricing_service/PricingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_pricing_engine.pricing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
