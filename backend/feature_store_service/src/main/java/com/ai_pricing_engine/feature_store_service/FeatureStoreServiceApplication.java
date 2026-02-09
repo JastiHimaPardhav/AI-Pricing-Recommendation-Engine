d72c46d39d202efee807b24196ded4f7b24247b2
JastiHimaPardhav <himapardhavjasti@gmail.com>
2026-02-09 22:51:18 +0530

    Feature store service skeleton with dependencies & properties

---
+++ backend/feature_store_service/src/main/java/com/ai_pricing_engine/feature_store_service/FeatureStoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ai_pricing_engine.feature_store_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureStoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureStoreServiceApplication.class, args);
+	}
+
+}
---
+++ backend/feature_store_service/src/test/java/com/ai_pricing_engine/feature_store_service/FeatureStoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_pricing_engine.feature_store_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureStoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
