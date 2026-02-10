d921b97b0eb9fe302d2634e93e40b60a6e2a6c5f
JastiHimaPardhav <himapardhavjasti@gmail.com>
2026-02-10 23:33:52 +0530

    Recommendation service skeleton with dependencies & properties

---
+++ backend/recommendation_service/src/test/java/com/ai_pricing_engine/recommendation_service/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_pricing_engine.recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
